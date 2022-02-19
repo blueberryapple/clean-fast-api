f492ff7dcc7d2dfdb86f24ab4fe65dfd554f4957
Ben Tea <9259833+blueberryapple@users.noreply.github.com>
2022-02-19 13:33:38 -0800

    make classifier a singleton

--- flower/repository.py
+++ flower/repository.py
@@ -5,6 +5,27 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 
 
+class FlowerClassifier:
+    """
+    Flower classifier singleton
+    """
+    iris_classifier = None
+
+    def __init__(self):
+        raise Exception('Please use get_instance() method')
+
+    @classmethod
+    def get_instance(cls) -> RandomForestClassifier:
+        """
+        Class method to obtain singleton flower classifier
+        """
+        if cls.iris_classifier is None:
+            with open('flower/model.pkl', 'rb') as iris_model:
+                cls.iris_classifier = pickle.load(iris_model)
+
+        return cls.iris_classifier
+
+
 class FlowerRepository:
     """
     Flower repository that uses the random forrest classifier
@@ -12,8 +33,7 @@ class FlowerRepository:
     iris_classifier: RandomForestClassifier
 
     def __init__(self) -> None:
-        with open('flower/model.pkl', 'rb') as iris_model:
-            self.iris_classifier = pickle.load(iris_model)
+        self.iris_classifier = FlowerClassifier.get_instance()
 
     def get_flower_prediction(self, flower_attributes: list[float]) -> list[int]:
         """
